e74c1efb4ad1c7cef8fe17139120f78b7e793efd
Luiz Augusto Wendt <luizaugustow_1@msn.com>
2019-01-26 02:38:01 -0200

    added LUT

---
+++ Assets/External/LUT/Plugin/Signals/UnityEventSignal.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace LUT
+{
+    public abstract class UnityEventSignal<T> : MonoBehaviour where T : ASignal, new()
+    {
+        [SerializeField]
+        private UnityEvent _onDispatch = new UnityEvent();
+
+        private void Start()
+        {
+            Signals.Get<T>().AddListener(Dispatch);
+        }
+
+        private void OnDisable()
+        {
+            Signals.Get<T>().RemoveListener(Dispatch);
+        }
+
+        private void Dispatch()
+        {
+            _onDispatch.Invoke();
+        }
+    }
+
+    public abstract class UnityEventSignal<Type, SignalType, UnityEventType> : MonoBehaviour
+        where SignalType : ASignal<Type>, new()
+        where UnityEventType : UnityEvent<Type>, new()
+    {
+        [SerializeField]
+        private UnityEventType _onDispatch = new UnityEventType();
+
+        private void Start()
+        {
+            Signals.Get<SignalType>().AddListener(Dispatch);
+        }
+
+        private void OnDisable()
+        {
+            Signals.Get<SignalType>().RemoveListener(Dispatch);
+        }
+
+        private void Dispatch(Type t)
+        {
+            _onDispatch.Invoke(t);
+        }
+    }
+}
---
+++ Assets/External/LUT/Plugin/Snippets/UI/TextVersion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace LUT.Snippets.UI
+{
+	public sealed class TextVersion : MonoBehaviour
+	{
+		[SerializeField]
+		[AutoFind(typeof(Text))]
+		private Text _text;
+
+        private void Reset()
+        {
+            _text = GetComponentInChildren<Text>();
+        }
+
+        private void OnEnable()
+		{
+			_text.text = "v" + Application.version;
+		}
+	}
+}
