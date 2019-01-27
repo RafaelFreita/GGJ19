273da29af1138cfc20714a0f0b9d8209b6fbe6ca
RafaelFreita <dfr.rafael@gmail.com>
2019-01-27 13:54:52 -0200

    Merge and adding pipe+trash

---
+++ Assets/Scripts/TrashCanTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCanTrigger : MonoBehaviour
+{
+
+    public ParticleSystem fire;
+    public GameObject virtualCamera;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        virtualCamera.SetActive(true);
+        fire.gameObject.SetActive(true);
+        fire.Play();
+        //Destroy(collision.gameObject);
+        collision.gameObject.GetComponentInChildren<ParticleSystem>().Stop(true);
+    }
+
+}
