64026ffd6aee61cd7cd39a5f18663180f7312f9d
RafaelFreita <dfr.rafael@gmail.com>
2019-01-27 10:38:49 -0200

    Finished level!

---
+++ Assets/Scripts/Last_Fireplace/LastFireplace.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastFireplace : MonoBehaviour
+{
+
+    public GameObject fireObject;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        fireObject.SetActive(true);
+        var particleSystems = collision.gameObject.GetComponentsInChildren<ParticleSystem>();
+        foreach(var p in particleSystems)
+        {
+            p.Play();
+        }
+    }
+}
